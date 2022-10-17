84675133fd8eda8beddd7e9f5959ed98510652fa
Enes CAN <enesscann98@gmail.com>
2022-10-17 12:54:08 +0300

    Project Euler - Multiples of 3 or 5

---
+++ src/main/kotlin/ProjectEuler/1_MultiplesOf3Or5.kt
@@ -0,0 +1,20 @@
+package ProjectEuler
+
+fun main() {
+    println(multiplesOfThreeOrFive())
+}
+
+fun multiplesOfThreeOrFive(): Long{
+    val number1 = 3
+    val number2 = 5
+    val smallestValue = 1
+    val greatestValue = 1000
+    var result: Long = 0
+
+    (smallestValue until greatestValue).forEach {
+        if (it % number1 == 0 || it % number2 == 0)
+            result += it
+    }
+
+    return result
+}
