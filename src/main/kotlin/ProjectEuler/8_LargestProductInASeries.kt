29b22b87c171f08ca84b9b819649d6c216ffe383
Enes CAN <enesscann98@gmail.com>
2023-01-31 07:51:58 +0300

    ProjectEuler - LargestProductInASeries

--- src/main/kotlin/ProjectEuler/8_LargestProductInASeries.kt
+++ src/main/kotlin/ProjectEuler/8_LargestProductInASeries.kt
@@ -1,6 +1,6 @@
 package ProjectEuler
 
-private fun main() {
+fun main() {
     val adjacent = 13
     val number = "73167176531330624919225119674426574742355349194934" +
             "96983520312774506326239578318016984801869478851843" +
